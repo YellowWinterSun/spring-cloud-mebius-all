76424b899593bc4a93fbfcd8f7318b87bbbcad95
mebius <448241091hh@gmail.com>
2020-12-05 20:20:43 +0800

    TEST: 测试一下这个分支

--- spring-cloud-mebius-all-eureka-server/src/main/java/dy/mebius/sample/sc/EurekaServerApplication.java
+++ spring-cloud-mebius-all-eureka-server/src/main/java/dy/mebius/sample/sc/EurekaServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * eureka server for git_branch test-1
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
