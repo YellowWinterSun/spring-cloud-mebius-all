0f2b27dc8b13c2e8a73705a8d006d7e7b8712475
mebius <448241091hh@gmail.com>
2020-12-05 20:39:24 +0800

    FEAT: 启动参数注释

--- spring-cloud-mebius-all-eureka-server/src/main/java/dy/mebius/sample/sc/EurekaServerApplication.java
+++ spring-cloud-mebius-all-eureka-server/src/main/java/dy/mebius/sample/sc/EurekaServerApplication.java
@@ -4,6 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 启动参数
+ * VM option:
+ * <div>
+ *      -XX:+UseG1GC
+ *      -XX:MaxGCPauseMillis=200
+ *      -XX:InitiatingHeapOccupancyPercent=45
+ *      -Xmx200M
+ *      -XX:+HeapDumpOnOutOfMemoryError
+ * </div>
+ *
+ * Program argument:
+ * <div>
+ *     --spring.profiles.active=cluster1
+ *     --spring.profiles.active=cluster2
+ * </div>
+ *
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
--- spring-cloud-mebius-all-hello-service/src/main/java/dy/mebius/sample/sc/hello/service/HelloServiceApplication.java
+++ spring-cloud-mebius-all-hello-service/src/main/java/dy/mebius/sample/sc/hello/service/HelloServiceApplication.java
@@ -6,6 +6,22 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * 启动参数
+ * VM option:
+ * <div>
+ *      -XX:+UseG1GC
+ *      -XX:MaxGCPauseMillis=200
+ *      -XX:InitiatingHeapOccupancyPercent=45
+ *      -Xmx512M
+ *      -XX:+HeapDumpOnOutOfMemoryError
+ * </div>
+ *
+ * Program argument:
+ * <div>
+ *     --server.port=8081
+ * </div>
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
